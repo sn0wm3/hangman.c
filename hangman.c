339207f5714c3384b35627d312c998f5a93dd581
Anabelle VanDenburgh <anabellevandenburgh@gmail.com>
2021-05-17 00:02:20 -0400

    add blanks to word

--- hangman.c
+++ hangman.c
@@ -6,15 +6,15 @@ std::string getStartWord() {
 		"test",
 		"foo",
 		"bar",
-		"home",
-		"queer",
+		"dave's home",
+		"queer house",
 	};
 
 	int arrayLen = 5;
 	return StartWords[ std::rand() % arrayLen]; //you might want to change this code to somethign else
 }
 
-int getStringLength(std::string inputString) {
+int strLen(std::string inputString) {
 	//TODO: support non-ascii chars (emoji, cjk, arabic, etc.)
 
 	//// these lines dont compile with g++ on debian buster
@@ -23,7 +23,49 @@ int getStringLength(std::string inputString) {
 	return inputString.length();
 }
 
+std::string toLower(std::string input){
+	std::string newString (strLen(input),' ');
+
+	for(int i = 0; i < input.length(); i++){
+		if('A' < input[i] && input[i] < 'Z')//if char is uppercase
+			newString[i] = input[i] - 40; //dirty ascii hack to lowercase
+		else
+			newString[i] = input[i];
+	}
+	return newString;
+}
+
 int main() {
-	std::string word = getStartWord();
-	std::cout << word << getStringLength(word);
+	bool playingGame = true;
+	int j=0;
+	while(playingGame) {
+		std::string secretWord = getStartWord();
+		std::string secretWordLower = toLower(secretWord);
+		std::string publicWord (strLen(secretWord),' '); //as large as secretword, but only spaces
+
+		for(int i = 0; i < secretWordLower.length(); i++) {
+			if (secretWordLower[i] < 'a' || 'z' < secretWordLower[i]) {//matches non-alphabetic charachters
+				publicWord[i] = secretWordLower[i]; //matching chars are "filled in" for the user
+			} else {
+				publicWord[i] = '_'; //non-maching chars are replaced with "blanks"
+			}
+		}
+
+		std::cout << publicWord << std::endl;
+
+		if(j++>10)break;
+
+		// int remainingGuesses = 5;
+		// int remainingLetters = /*???*/;
+		// char* guessedLetters = /*???*/
+
+		// //main loop
+		// while(remainingGuesses > 0 && remainingLetters > 0){
+
+		// }
+
+		// //ask user if they want to continue
+		// std::cout << "Do you want to continue? (y/n) ";
+		// std::cin >
+	}
 }
